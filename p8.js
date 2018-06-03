d36decb88b000ef0bdc64898787261c3afbd6195
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2018-06-04 08:26:17 +0900

    別のファイルを追加

---
+++ p8.js
@@ -0,0 +1,14 @@
+const EPS = 0.0001;    // 許容誤差
+
+function main() {
+    console.log("x^3 + x - 1 のニュートン法による数値計算");
+
+    var a = 1.0;
+    console.log("初期値 a=" + a);
+
+    var b;
+
+    // ここにニュートン法による演算の主要部分を記述する
+
+    console.log("近似解 x = " + b);
+}
