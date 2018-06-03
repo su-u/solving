72e45b84f184bbac2043352b95f45580fa71711c
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2018-06-04 08:25:30 +0900

    ファイルを追加

---
+++ function_definition.js
@@ -0,0 +1,9 @@
+// 関数 y = x^3+x-1 定義
+function func_y( x ) {
+    return Math.pow( x, 3.0 ) + x - 1.0;
+}
+
+// 関数 y' = 3x^2+1 定義
+function func_z( x ) {
+    return 3.0 * Math.pow( x, 2.0 ) + 1.0;
+}
