a1b5f76623d8efba791b3122d686d2fd5d997cdf
unknown <F1732087>
2019-06-03 15:57:38 +0900

    self update

--- p3.js
+++ p3.js
@@ -1,22 +1,31 @@
 const EPS = 0.0001; // constは定数の定義
 
 function main() {
-    var a = 0.0, b = 1.0;  // 初期値
+    let a = 0.0,
+        b = 1.0; // 初期値
 
-    console.log("x^3 + x - 1 の2分法による数値計算");
-    console.log("初期値 a=" + a);
-    console.log("初期値 b=" + b);
-    var x = nibun(a, b);  // 解
-    console.log("近似解 x = " + x);
+    console.log('x^3 + x - 1 の2分法による数値計算');
+    console.log('初期値 a=' + a);
+    console.log('初期値 b=' + b);
+    let x = nibun(a, b); // 解
+    console.log('近似解 x = ' + x);
 }
 
-function nibun(a, b) {
-    var c;
-
+const nibun = (a, b) => {
+    let count = 1;
+    let c;
+    let output = document.querySelector('#output');
     do {
-        c = (a + b) / 2.0;  // 2分計算
-        if ((func_y(c) * func_y(a)) < 0) b = c;  // 式(1.2)
-        else a = c;  // 式(1.3)
-    } while (Math.abs(a - b) > EPS);  // 収束判別　式(1.4)の変形
+        c = (a + b) / 2.0; // 2分計算
+        const str = document.createTextNode(count + ':' + c);
+        const p = document.createElement('p');
+        p.appendChild(str);
+        output.appendChild(p);
+        console.log(c);
+        if (func_y(c) * func_y(a) < 0) b = c;
+        // 式(1.2)
+        else a = c; // 式(1.3)
+        count += 1;
+    } while (Math.abs(a - b) > EPS); // 収束判別　式(1.4)の変形
     return c;
-}
+};
--- p8.js
+++ p8.js
@@ -1,14 +1,32 @@
-const EPS = 0.0001;    // 許容誤差
+const EPS = 0.0001; // 許容誤差
 
 function main() {
-    console.log("x^3 + x - 1 のニュートン法による数値計算");
+    console.log('x^3 + x - 1 のニュートン法による数値計算');
 
-    var a = 1.0;
-    console.log("初期値 a=" + a);
+    let a = 1.0;
+    console.log('初期値 a=' + a);
 
-    var b;
+    let b = newton(a);
 
     // ここにニュートン法による演算の主要部分を記述する
 
-    console.log("近似解 x = " + b);
+    console.log('近似解 x = ' + b);
 }
+
+const newton = a => {
+    let c;
+    let count = 1;
+    while (1) {
+        c = a - func_y(a) / func_z(a);
+        const str = document.createTextNode(`${count}:${c}`);
+        const p = document.createElement('p');
+        p.appendChild(str);
+        output.appendChild(p);
+        console.log(c);
+        if (Math.abs(a - c) < EPS) break;
+        else a = c;
+
+        count += 1;
+    }
+    return c;
+};
